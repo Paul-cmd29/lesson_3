9e38d2220cbed81a36967078d316e63e328efbf8
Paul-cmd29 <paulpetrov8510@gmail.com>
2022-08-04 16:15:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int sum = 0;
+
+Console.WriteLine("Enter x:");
+string? strnum = Console.ReadLine();
+bool isTrue = int.TryParse(strnum, out int x);
+Console.WriteLine("Enter y: ");
+string? strnum1 = Console.ReadLine();
+bool isTrue1 = int.TryParse(strnum1, out int y);
+
+if (isTrue == true && isTrue1 == true)
+{
+    for (int i = x; i < y + 1; i++)
+    {
+        sum += i;
+
+    }
+    Console.WriteLine(sum);
+    
+}
+
+else
+{
+    Console.WriteLine("Inccorect!");
+}
+
+
